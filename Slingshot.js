a9a47cb80fc55a3f02d91b5f2d58df0adff89909
sohumtokekar <sohumtokekar@gmail.com>
2020-11-05 19:44:33 -0600

    Slingshot images and rubber band

--- Slingshot.js
+++ Slingshot.js
@@ -7,6 +7,9 @@ class SlingShot{
             length: 10
         }
         this.pointB = pointB
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,11 +19,34 @@ class SlingShot{
     }
 
     display(){
+
+        //catapult images
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            //rubber band 
+            push();
+            if(pointA.x < 220){
+                strokeWeight(7);
+                stroke(48,22,8);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+                //block for bird to rest on
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            }
+            else{
+                strokeWeight(3);
+                //stroke(48,22,8);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+                //block for bird to rest on
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+            }
+            
+            pop();
         }
     }
     
